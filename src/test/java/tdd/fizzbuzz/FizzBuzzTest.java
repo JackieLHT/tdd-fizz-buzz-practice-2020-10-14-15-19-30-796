0b580813ecebd1a4253eacc83b3e2f284c306748
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-26 14:02:57 +0800

    [tdd-fizz-buzz][Jackie] feat: implemented normal number case test

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,15 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void shoud_output_order_nubmer_string_when_common_order_given() throws Exception {
+        int order = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String actual = fizzBuzz.countOff(order);
+        assertEquals("1",actual);
+    }
+
 }
