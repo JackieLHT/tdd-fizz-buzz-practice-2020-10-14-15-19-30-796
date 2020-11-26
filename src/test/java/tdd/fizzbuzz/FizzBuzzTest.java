312fb3c86c9c3eca4d6ccdf9c04a4a8ce5e6aade
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-26 14:07:13 +0800

    [tdd-fizz-buzz][Jackie] feat: implemented normal number case in countOff

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
     @Test
-    public void shoud_output_order_nubmer_string_when_common_order_given() throws Exception {
+    public void should_return_order_nubmer_string_when_countOff_given_normal_order() throws Exception {
         int order = 1;
         FizzBuzz fizzBuzz = new FizzBuzz();
         String actual = fizzBuzz.countOff(order);
